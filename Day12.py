eb1f3d6e918ea5277b053ca9b5eba1a7da3f8909
Yohana Ramadan <yohanaramadan01@gmail.com>
2026-07-06 08:27:30 +0100

    Create simple age calculator program in Python

--- Day12.py
+++ Day12.py
@@ -1,43 +1,9 @@
-expenses = []
+from datetime import date
 
-while True:
-    print("\n=== Expense Tracker ===")
-    print("1. Add Expense")
-    print("2. View Expenses")
-    print("3. Show Total")
-    print("4. Exit")
+current_year = date.today().year
 
-    choice = input("Choose an option: ")
+birth_year = int(input("Enter your birth year: "))
 
-    if choice == "1":
-        amount = float(input("Enter amount: "))
-        category = input("Enter category: ")
+age = current_year - birth_year
 
-        expenses.append({
-            "amount": amount,
-            "category": category
-        })
-
-        print("Expense added successfully!")
-
-    elif choice == "2":
-        if not expenses:
-            print("No expenses recorded.")
-        else:
-            print("\nExpenses:")
-            for i, expense in enumerate(expenses, start=1):
-                print(
-                    f"{i}. Amount: ${expense['amount']} | "
-                    f"Category: {expense['category']}"
-                )
-
-    elif choice == "3":
-        total = sum(expense["amount"] for expense in expenses)
-        print(f"\nTotal Expenses: ${total:.2f}")
-
-    elif choice == "4":
-        print("Goodbye!")
-        break
-
-    else:
-        print("Invalid option.")
+print(f"You are {age} years old.")
