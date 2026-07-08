1c8435c9af0b094822c533e589f9b46732c75fb5
Yohana Ramadan <yohanaramadan01@gmail.com>
2026-07-08 12:19:53 +0100

    Create random quote generator

---
+++ app14.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Keep learning.",
+    "Practice makes progress.",
+    "Code. Debug. Repeat.",
+    "Small steps every day.",
+    "Consistency beats talent."
+]
+
+print(random.choice(quotes))
